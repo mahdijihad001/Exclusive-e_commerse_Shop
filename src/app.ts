8a7baa9e629ecd25d8b40cfe315166d1906ba2af
mahdijihad001 <mahdijihad001@gmail.com>
2025-08-18 00:37:04 +0600

    Handle Zod & mongoose errors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { type Request, type Response } from "express";
 export const app = express();
 import cors from "cors";
 import userRouter from "./app/modules/user/user.router";
+import { globalErrorHanle } from "./app/middleware/gbalErrorHandle";
+import { notFoundRoute } from "./app/middleware/notFoundRoute";
 
 // Middleware
 
@@ -15,3 +17,7 @@ app.use("/user" , userRouter);
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({ success: true, message: "Exclusive E-commerce shop server runing successfully!" });
 });
+
+
+app.use(globalErrorHanle);
+app.use(notFoundRoute);
--- src/app/middleware/gbalErrorHandle.ts
+++ src/app/middleware/gbalErrorHandle.ts
@@ -1,28 +1,59 @@
 import type { NextFunction, Request, Response } from "express";
-import { envVer } from "../config/env.js";
-import { AppError } from "../utils/AppError.js";
+import { ZodError } from "zod";
+import { AppError } from "../utils/AppError";
+import { envVer } from "../config/env";
 
 export const globalErrorHanle = (err: any, req: Request, res: Response, next: NextFunction) => {
 
-    if (envVer.DEVELOPMENT_ENVIRONMENT === "development") {
-        console.log(err);
-    }
+    // if (envVer.DEVELOPMENT_ENVIRONMENT === "development") {
+    //     console.log(err);
+    // }
 
     let stausCode = 500;
     let message = `Something went wrong.`;
+    let errorStore: any = [];
 
-    if (err instanceof AppError) {
+
+    //----------- Zod Error Handle-------------
+    if (err instanceof ZodError) {
+        const error = err.issues;
+        error.forEach((issue) =>{
+            errorStore.push({
+                path : issue.path[issue.path.length - 1],
+                message : issue.message
+            })
+        });
+
+        message = "Zod Error Validation",
+        stausCode = 400
+    }
+    // ------- Mongoose duplicate Error
+    else if(err.code === 11000){
+        const duplicate = err.message.match(/"([^"]*)"/);
+        message = `${duplicate[1]} already exist!`;
+        stausCode = 400;
+    }
+    // Mongoose Cast error
+    else if(err.name === "CastError"){
+        stausCode = 400;
+        message = "Invalid mongoDB Object id. Please provide a valid id."
+    }
+    // ------------- Custom App error handle --------------
+    else if (err instanceof AppError) {
         stausCode = err.statusCode;
         message = err.message
-    } else if (err instanceof Error) {
+    }
+    // javascript default error Handle 
+    else if (err instanceof Error) {
         stausCode = 500;
         message = err.message
-    };
+    }
 
 
     res.status(stausCode).json({
         success: false,
         message: message,
+        errorStore,
         err: envVer.DEVELOPMENT_ENVIRONMENT === "development" ? err : null,
         stack: envVer.DEVELOPMENT_ENVIRONMENT === "development" ? err.stack : null
     })
