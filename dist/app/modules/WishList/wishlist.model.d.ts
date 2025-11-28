bd5287733d260114aea497ab0d0715d9bc621af5
mahdijihad001 <mahdijihad001@gmail.com>
2025-11-28 14:55:42 +0600

    Update

--- dist/app/modules/WishList/wishlist.model.d.ts
+++ dist/app/modules/WishList/wishlist.model.d.ts
@@ -1,2 +1,53 @@
-export {};
+import mongoose from "mongoose";
+export declare const Wishlist: mongoose.Model<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+}, {}, {}, {}, mongoose.Document<unknown, {}, {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+}, {}, {
+    timestamps: true;
+}> & {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+} & {
+    _id: mongoose.Types.ObjectId;
+} & {
+    __v: number;
+}, mongoose.Schema<any, mongoose.Model<any, any, any, any, any, any>, {}, {}, {}, {}, {
+    timestamps: true;
+}, {
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+}, mongoose.Document<unknown, {}, mongoose.FlatRecord<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+}>, {}, mongoose.ResolveSchemaOptions<{
+    timestamps: true;
+}>> & mongoose.FlatRecord<{
+    createdAt: NativeDate;
+    updatedAt: NativeDate;
+} & {
+    user: mongoose.Types.ObjectId;
+    productId?: mongoose.Types.ObjectId | null;
+}> & {
+    _id: mongoose.Types.ObjectId;
+} & {
+    __v: number;
+}>>;
 //# sourceMappingURL=wishlist.model.d.ts.map
